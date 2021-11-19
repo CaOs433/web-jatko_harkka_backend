2ba0a7e490c552ce4bbd3404c02790f3afba1e4e
Oskari Saarinen <saarinenoskari13@gmail.com>
2021-11-19 20:44:54 +0200

    add dummy data for testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,6 +13,15 @@ app.use(express.static("public"));
 /*app.get("/", (req, res) => {
     res.send("Hello World!");
 });*/
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'), function (err) {
+        if (err) {
+            console.log('Error');
+            res.status(500).send(err);
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example port listening at http://localhost:${port}`);
