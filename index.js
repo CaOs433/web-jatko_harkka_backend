89ead95153f5a7391cbebcdf7c8396e55443e952
Oskari Saarinen <saarinenoskari13@gmail.com>
2021-11-30 00:39:18 +0200

    2nd release

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-//const fetch = require('node-fetch');
+const request = require('request');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -20,35 +20,54 @@ app.use(express.static("public"));
     return response.json();
 }*/
 
-const callBackFunc = function (err) {
+const callBackOnError = function (err) {
     if (err) {
         console.log('Error');
         res.status(500).send(err);
     }
 };
 
+const options = (endfix) => {
+    return {
+        'method': 'GET',
+        'url': 'api.coincap.io/v2/'+endfix,
+        'headers': {}
+    };
+};
+
 app.get("/get/assets*", function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/assets.json'), callBackFunc);
+    res.sendFile(path.join(__dirname, 'public/assets.json'), callBackOnError);
+    //req.pipe(request("https://api.coincap.io/v2/assets")).pipe(res);
+    /*request(options("assets"), function (error, response, _res = res) {
+        callBackOnError(error);
+        console.log(response.body);
+
+        if (response.body.includes("You exceeded your 200 request(s) rate limit of your FREE plan")) {
+            _res.sendFile(path.join(__dirname, 'public/assets.json'), callBackOnError);
+        } else {
+            _res.send(response.body);
+        }
+    });*/
 });
 
 app.get("/get/history*", function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/history.json'), callBackFunc);
+    res.sendFile(path.join(__dirname, 'public/history.json'), callBackOnError);
 });
 
 app.get("/get/markets*", function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/markets.json'), callBackFunc);
+    res.sendFile(path.join(__dirname, 'public/markets.json'), callBackOnError);
 });
 
 app.get("/get/exchanges*", function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/exchanges.json'), callBackFunc);
+    res.sendFile(path.join(__dirname, 'public/exchanges.json'), callBackOnError);
 });
 
 app.get("/get/rates*", function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/rates.json'), callBackFunc);
+    res.sendFile(path.join(__dirname, 'public/rates.json'), callBackOnError);
 });
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'), callBackFunc);
+    res.sendFile(path.join(__dirname, 'public/index.html'), callBackOnError);
 });
 
 /*app.get("/get/assets", (req, res) => {
